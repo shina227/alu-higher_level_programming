fa4765a66fa3f8c5809281fa0868698d6b7ec340
Shina <m.atete@alustudent.com>
2024-10-22 09:53:28 +0200

    Task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 # Print the ASCII alphabet in lowercase without a new line
-print("{}".format("abcdefghijklmnopqrstuvwxyz"), end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
