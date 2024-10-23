00c57b00bd75c256cc572ca3e20d551ff070e700
Shina <m.atete@alustudent.com>
2024-10-23 10:12:29 +0200

    Task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
-    """Prints numbers from 1 to 100 with Fizz for multiples of 3, Buzz for multiples of 5, and FizzBuzz for multiples of both 3 and 5."""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="  ")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
             print(i, end=" ")
+    print()  # Print a newline at the end
