8fd59bd20e6f2e5c5d5f8d7ff4ff3c5db19c989f
Shina <m.atete@alustudent.com>
2024-10-23 15:54:39 +0200

    Task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
-from calculator_1 import add, sub, mul, div
-
-# Define the variables
-a = 10
-b = 5
-
-# Call the functions and print the results
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
