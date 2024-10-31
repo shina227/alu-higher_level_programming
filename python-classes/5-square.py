09b92e30eb851a6738cca38cdb653c7bd6249b59
Shina <m.atete@alustudent.com>
2024-10-31 14:18:44 +0200

    Task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a new square.
+
+        Args:
+            size (init): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with the character '#'."""
+        if self.size == 0:
+            print("")
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
