646c5d7e5fdded5e78d8c41eedc71a45120b4687
Shina <m.atete@alustudent.com>
2024-10-31 09:41:21 +0200

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represent a square"""
+
+    
+    def __init__(self, size):
+        """Initialize a square with a private size attribute"""
+        self.__size = size
