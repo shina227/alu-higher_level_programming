92f6cb0e42db885225df3824669047f139c09745
Shina <m.atete@alustudent.com>
2024-11-15 15:36:51 +0200

    Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,20 @@
--- list privileges
+-- This script lists all privileges of the MySQL users
+user_0d_1 and user_0d_2 on localhost.
+
+-- Create user user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Create user user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant all privileges to user_0d_2
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Show grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
