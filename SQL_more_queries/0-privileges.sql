65493c5346bb03e1d1fd5b01ff81b55cd7db07f3
shina227 <m.atete@alustudent.com>
2024-11-15 10:26:57 -0800

    Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,8 @@
--- lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Lists all privileges of users 'user_0d_1' and 'user_0d_2' on localhost
+
+-- Check and display privileges for 'user_0d_1'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Check and display privileges for 'user_0d_2'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
