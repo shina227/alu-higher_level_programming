89c46482e1e0d513ef0adf8bac44167a682aa1ea
Shina <m.atete@alustudent.com>
2024-11-15 15:50:25 +0200

    Tasks

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,4 @@
--- This script checks for the existence of MySQL users
-user_0d_1 and user_0d_2 on localhost
--- and lists their privileges.
+-- Show all privileges of data users
 
--- Check if user_0d_1 exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1'
-AND host = 'localhost') AS user_exists;
-
--- Check if user_0d_2 exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_2'
-AND host = 'localhost') AS user_exists;
-
--- Show grants for user_0d_1 if it exists
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Show grants for user_0d_2 if it exists
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
