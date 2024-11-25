f3a502c693c5b1435f6b4f0063aed6414f718916
Shina <m.atete@alustudent.com>
2024-11-25 19:55:49 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" This script fetches the status from a specified URL and
-displays the response details """
+""" Python script that fetches a url """
+
 
 import urllib.request
 
+
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
-
+    url = (
+            'https://alu-intranet.hbtn.io/status'
+    )
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-
-        output = (
-            "Body response:\n"
-            f"\t- type: {type(body)}\n"
-            f"\t- content: {body}\n"
-            f"\t- utf8 content: {body.decode('utf-8')}\n"
-        )
-
-        print(output)
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
