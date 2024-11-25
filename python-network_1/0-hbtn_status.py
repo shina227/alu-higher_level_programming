ec94a039a1b91a51722474aca00dde58fbd49bef
Shina <m.atete@alustudent.com>
2024-11-25 11:11:55 +0200

    New 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """  fetches https://alu-intranet.hbtn.io/status  """
+
 import urllib.request
 
-url = 'https://intranet.hbtn.io/status'
-if url.startswith('https://'):
-    url = "https://alu-intranet.hbtn.io/status"
 
 if __name__ == "__main__":
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-    content = response.read()
+    url = "https://alu-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+
     print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-    print("\t- utf8 content:", content.decode("utf-8"))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
