103f3a378f9fe686c302aa512b559d7b6d882d12
Shina <m.atete@alustudent.com>
2024-11-27 09:19:31 +0200

    Task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
